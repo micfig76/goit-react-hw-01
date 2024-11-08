f99e705689e22ae2c9a08ee381c73320d176044b
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-11-08 17:50:33 +0100

    styled profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,26 +2,26 @@ import css from "./Profile.module.css";
 
 export default function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
+    <div className={css.slate}>
+      <div className={css.personal}>
         <img className={css.avatar} src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <p className={css.profileName}>{name}</p>
+        <p className={css.nick}>@{tag}</p>
+        <p className={css.loco}>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={css.changables}>
+        <li className={css.follow}>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={css.val}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.view}>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={css.val}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.like}>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={css.val}>{stats.likes}</span>
         </li>
       </ul>
     </div>
