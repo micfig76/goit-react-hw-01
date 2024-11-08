a75bcaa2bdf52a6aa33c79ad9f48636823c77bd2
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-11-08 19:33:28 +0100

    styled transaction component

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,24 +2,26 @@ import css from "./TransactionHistory.module.css";
 
 export default function TransactionHistory({ items }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {items.map((item) => (
-          <tr key={item.id}>
-            <td className={css.type}>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
+    <div className={css.account}>
+      <table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {items.map((item) => (
+            <tr key={item.id}>
+              <td className={css.type}>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
